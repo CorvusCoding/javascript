9f97a85f6819cf7534f54d73b4dafa3b70b6d34b
CorvusCoding <sidneybartlett13@gmail.com>
2017-03-28 05:09:08 -0700

    added all

---
+++ randomInCreate.js
@@ -0,0 +1,19 @@
+var randomInterger = function(val) {
+	var ival = Math.random()*256;
+	//console.log(ival);
+	ival = Math.floor(ival);
+	return ival
+
+}
+// main
+var rInt ;
+var i;
+var erik = 0
+for (i = 0; i < 65536; i++){
+	rInt 	= randomInterger();
+	if (rInt == 6)erik++;
+	console.log(rInt+",");
+	process.stdout.write(rInt+",");
+	
+}
+console.log("\n\n Counting 8 = "+erik);
